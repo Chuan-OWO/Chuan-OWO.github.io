1d2908f9b0143354d3705985285dc501707c6c2e
chuan <chihchuan.hsu316@gmail.com>
2025-06-12 17:02:46 +0800

    ddddddd

--- script3.js
+++ script3.js
@@ -34,7 +34,7 @@ var models = [
     {
         url: './assets/dragonite/scene.gltf',
         scale: '0.08 0.08 0.08',
-        rotation: '0 180 0',
+        rotation: '0 0 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
 ];
